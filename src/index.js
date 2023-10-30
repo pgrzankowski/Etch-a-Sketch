353f41d850dafd55c2840e4e30fb0f3634bb918d
pgrzankowski <pawel0grzankowski@gmail.com>
2023-10-30 13:48:28 +0100

    Create grid scaling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const gridContainer = document.querySelector("#gridContainer");
 const clearBtn = document.querySelector("#clearBtn");
+const gridSizeLabel = document.querySelector("#gridSizeLabel");
+const sizeSlider = document.querySelector("#sizeSlider");
 
-const grid = [];
-const gridSize = 16;
+let grid = [];
+let gridSize = 16;
+
+
+sizeSlider.onchange = () => {
+    let a = sizeSlider.value;
+    gridSizeLabel.textContent = `${a}x${a}`;
+    gridSize = a;
+    createGrid()
+    console.log(gridSize);
+}
 
 
 function createGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+    grid = [];
     for (let i=0; i<gridSize; i++) {
         grid[i] = [];
         for (let j=0; j<gridSize; j++) {
